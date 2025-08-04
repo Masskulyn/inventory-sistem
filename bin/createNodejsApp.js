70d41b4e5329f8d854b771dba1e050e085517b03
Masskulyn <anggitrangga123@gmail.com>
2025-08-04 07:13:59 +0700

    SUSUAN

--- bin/createNodejsApp.js
+++ bin/createNodejsApp.js
@@ -1,111 +1,57 @@
-    #!/usr/bin/env node
+#!/usr/bin/env node
+
 const util = require('util');
 const path = require('path');
 const fs = require('fs');
-const { execSync } = require('child_process');
+const { exec, execSync } = require('child_process');
 
-// Utility functions
-const exec = util.promisify(require('child_process').exec);
-async function runCmd(command) {
-  try {
-    const { stdout, stderr } = await exec(command);
-    console.log(stdout);
-    console.log(stderr);
-  } catch {
-    (error) => {
-      console.log(error);
-    };
-  }
-}
+// Promisify exec
+const runCmd = util.promisify(exec);
 
+// Cek apakah user punya yarn
 async function hasYarn() {
   try {
-    await execSync('yarnpkg --version', { stdio: 'ignore' });
+    execSync('yarnpkg --version', { stdio: 'ignore' });
     return true;
   } catch {
     return false;
   }
 }
 
-// Validate arguments
+// Validasi argumen (nama folder project)
 if (process.argv.length < 3) {
-  console.log('Please specify the target project directory.');
-  console.log('For example:');
+  console.log('❗ Harap tentukan nama folder project.');
+  console.log('Contoh:');
   console.log('    npx create-nodejs-app my-app');
-  console.log('    OR');
-  console.log('    npm init nodejs-app my-app');
   process.exit(1);
 }
 
-// Define constants
-const ownPath = process.cwd();
+// Setup path
 const folderName = process.argv[2];
+const ownPath = process.cwd();
 const appPath = path.join(ownPath, folderName);
 const repo = 'https://github.com/zexoverz/inventory-system';
 
-// Check if directory already exists
-try {
-  fs.mkdirSync(appPath);
-} catch (err) {
-  if (err.code === 'EEXIST') {
-    console.log('Directory already exists. Please choose another name for the project.');
-  } else {
-    console.log(err);
-  }
+// Cek apakah folder sudah ada
+if (fs.existsSync(appPath)) {
+  console.error('❗ Folder sudah ada. Gunakan nama lain.');
   process.exit(1);
 }
 
+// Mulai setup
 async function setup() {
   try {
-    // Clone repo
-    console.log(`Downloading files from repo ${repo}`);
+    console.log(`🚀 Meng-clone project dari ${repo}`);
     await runCmd(`git clone --depth 1 ${repo} ${folderName}`);
-    console.log('Cloned successfully.');
-    console.log('');
+    console.log('✅ Clone berhasil.\n');
 
-    // Change directory
     process.chdir(appPath);
 
     // Install dependencies
     const useYarn = await hasYarn();
-    console.log('Installing dependencies...');
-    if (useYarn) {
-      await runCmd('yarn install');
-    } else {
-      await runCmd('npm install');
-    }
-    console.log('Dependencies installed successfully.');
-    console.log();
+    console.log('📦 Menginstall dependencies...');
+    await runCmd(useYarn ? 'yarn install' : 'npm install');
+    console.log('✅ Dependencies berhasil diinstall.\n');
 
-    // Copy envornment variables
-    fs.copyFileSync(path.join(appPath, '.env.example'), path.join(appPath, '.env'));
-    console.log('Environment files copied.');
-
-    // Delete .git folder
-    await runCmd('npx rimraf ./.git');
-
-    // Remove extra files
-    fs.unlinkSync(path.join(appPath, 'CHANGELOG.md'));
-    fs.unlinkSync(path.join(appPath, 'CODE_OF_CONDUCT.md'));
-    fs.unlinkSync(path.join(appPath, 'CONTRIBUTING.md'));
-    fs.unlinkSync(path.join(appPath, 'bin', 'createNodejsApp.js'));
-    fs.rmdirSync(path.join(appPath, 'bin'));
-    if (!useYarn) {
-      fs.unlinkSync(path.join(appPath, 'yarn.lock'));
-    }
-
-    console.log('Installation is now complete!');
-    console.log();
-
-    console.log('We suggest that you start by typing:');
-    console.log(`    cd ${folderName}`);
-    console.log(useYarn ? '    yarn dev' : '    npm run dev');
-    console.log();
-    console.log('Enjoy your production-ready Node.js app, which already supports a large number of ready-made features!');
-    console.log('Check README.md for more info.');
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-setup();
+    // Salin file .env.example ke .env jika ada
+    c
